7aa65ffb3431431f92bdfb2e4b39c0c4907eb12c
Samson Cheung <samsonc89@yahoo.com>
2022-11-29 12:20:12 -0800

    Create function to generate nav buttons

--- src/modules/load.js
+++ src/modules/load.js
@@ -1,3 +1,45 @@
 export default function printMe() {
   console.log("Yee Haw! I get called from load.js!");
 }
+
+ function loadNav(){
+
+  const navBar = document.createElement('div')
+
+
+  navBar.id = 'navbar'
+
+  //append navbar
+  navBar.appendChild(createNav())
+
+  return navBar
+}
+
+
+function createNav (){
+const div = document.createElement('div');
+
+  const homeButton = document.createElement('button');
+
+  homeButton.classList.add('navButton');
+  homeButton.textContent = 'Home';
+
+
+  const menuButton = document.createElement('button');
+  menuButton.classList.add('navButton');
+  menuButton.textContent = 'Menu';
+
+
+  const contactButton = document.createElement('button');
+  contactButton.classList.add('navButton');
+  contactButton.textContent = 'Contact';
+
+div.appendChild(homeButton)
+div.appendChild(menuButton)
+div.appendChild(contactButton)
+
+  return div 
+    
+  }
+
+export { loadNav }
