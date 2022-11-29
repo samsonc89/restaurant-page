153982c22b0e0e472a80502db001729186964564
Samson Cheung <samsonc89@yahoo.com>
2022-11-29 12:36:50 -0800

    Refactor unncessary div creation

--- src/modules/load.js
+++ src/modules/load.js
@@ -2,44 +2,35 @@ export default function printMe() {
   console.log("Yee Haw! I get called from load.js!");
 }
 
- function loadNav(){
+ function loadPage(){
 
   const navBar = document.createElement('div')
 
-
   navBar.id = 'navbar'
 
-  //append navbar
-  navBar.appendChild(createNav())
 
-  return navBar
-}
+    const homeButton = document.createElement('button');
+    
+    homeButton.classList.add('navButton');
+    homeButton.textContent = 'Home';
+    
+    
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('navButton');
+    menuButton.textContent = 'Menu';
+    
+    
+    const contactButton = document.createElement('button');
+    contactButton.classList.add('navButton');
+    contactButton.textContent = 'Contact';
+    
+    navBar.appendChild(homeButton)
+    navBar.appendChild(menuButton)
+    navBar.appendChild(contactButton)
+    
 
-
-function createNav (){
-const div = document.createElement('div');
-
-  const homeButton = document.createElement('button');
-
-  homeButton.classList.add('navButton');
-  homeButton.textContent = 'Home';
-
-
-  const menuButton = document.createElement('button');
-  menuButton.classList.add('navButton');
-  menuButton.textContent = 'Menu';
-
-
-  const contactButton = document.createElement('button');
-  contactButton.classList.add('navButton');
-  contactButton.textContent = 'Contact';
-
-div.appendChild(homeButton)
-div.appendChild(menuButton)
-div.appendChild(contactButton)
-
-  return div 
+    return navBar
     
   }
 
-export { loadNav }
+export { loadPage }
