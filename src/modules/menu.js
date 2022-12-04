ef88fa50a531f2d306837ed84f50a174944c4a28
Samson Cheung <samsonc89@yahoo.com>
2022-12-03 18:05:26 -0800

    Add function to change content when menu button is clicked

---
+++ src/modules/menu.js
@@ -0,0 +1,12 @@
+function loadMenu (){
+
+    //clear current content 
+    
+
+    const mainWrapper = document.querySelector('#main-wrapper')
+    mainWrapper.textContent = "Menu testing"
+
+    return mainWrapper
+}
+
+export  {loadMenu}
