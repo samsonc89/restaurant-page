7a7a105b94b411e4c46dc656c0002c473bed6868
Samson Cheung <samsonc89@yahoo.com>
2022-12-03 18:15:04 -0800

    Write function to load contact page

---
+++ src/modules/contact.js
@@ -0,0 +1,11 @@
+function loadContact (){
+
+    //clear current content 
+
+
+    const mainWrapper = document.querySelector('#main-wrapper')
+    mainWrapper.textContent = "Contact testing"
+
+}
+
+export  {loadContact}
