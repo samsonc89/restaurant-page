de23e36a413088a77de13eef872ebc22fb727ebd
Samson Cheung <samsonc89@yahoo.com>
2022-12-08 13:23:38 -0800

    Add function to load home content

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { loadWrapper } from "./modules/load.js";
 import "./style.css";
 import { loadNav } from "./modules/load.js";
+import { loadHome } from "./modules/home.js";
 
 const content = document.querySelector("#content");
 
 content.prepend(loadNav());
-
 content.appendChild(loadWrapper());
+loadHome();
