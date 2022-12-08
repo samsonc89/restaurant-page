f464941de16340cc474363328607d66e9feac876
Samson Cheung <samsonc89@yahoo.com>
2022-12-08 12:48:13 -0800

    Remove commented placeholder code

--- src/index.js
+++ src/index.js
@@ -7,20 +7,3 @@ const content = document.querySelector("#content");
 content.prepend(loadNav());
 
 content.appendChild(loadWrapper());
-
-// const content = document.createElement("div");
-// content.id = "content";
-// content.innerHTML = `
-// <div id="header">
-//   <div id="navbar">
-//     <ul>
-//       <li>Home</li>
-//       <li>Menu</li>
-//       <li>Contact</li>
-//     </ul>
-//   </div>
-// </div>
-
-// `;
-
-// document.body.appendChild(content);
