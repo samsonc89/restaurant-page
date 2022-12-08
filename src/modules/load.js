426e3d2d19e4ae9c7e94b43e199c1b25294ff8ec
Samson Cheung <samsonc89@yahoo.com>
2022-12-08 13:22:13 -0800

    Remove initial placeholder text

--- src/modules/load.js
+++ src/modules/load.js
@@ -1,50 +1,43 @@
 export default function printMe() {
   console.log("Yee Haw! I get called from load.js!");
 }
-import {loadHome} from './home.js';
-import {loadMenu} from './menu.js'
-import {loadContact} from './contact.js'
+import { loadHome } from "./home.js";
+import { loadMenu } from "./menu.js";
+import { loadContact } from "./contact.js";
 
+function loadNav() {
+  const navBar = document.createElement("div");
+  navBar.id = "navbar";
 
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("navButton");
+  homeButton.textContent = "Home";
+  homeButton.addEventListener("click", loadHome);
 
- function loadNav(){
-  const navBar = document.createElement('div')
-  navBar.id = 'navbar'
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("navButton");
+  menuButton.textContent = "Menu";
+  menuButton.addEventListener("click", loadMenu);
 
-    const homeButton = document.createElement('button');    
-    homeButton.classList.add('navButton');
-    homeButton.textContent = 'Home';
-    homeButton.addEventListener('click', loadHome
-    )
-    
-    const menuButton = document.createElement('button');
-    menuButton.classList.add('navButton');
-    menuButton.textContent = 'Menu';
-    menuButton.addEventListener('click', loadMenu)
-    
-    const contactButton = document.createElement('button');
-    contactButton.classList.add('navButton');
-    contactButton.textContent = 'Contact';
-    contactButton.addEventListener('click', loadContact);
-    
-    navBar.appendChild(homeButton)
-    navBar.appendChild(menuButton)
-    navBar.appendChild(contactButton)
-    
-    return navBar
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("navButton");
+  contactButton.textContent = "Contact";
+  contactButton.addEventListener("click", loadContact);
 
-  }
+  navBar.appendChild(homeButton);
+  navBar.appendChild(menuButton);
+  navBar.appendChild(contactButton);
 
-  function loadWrapper (){
-
-    //clear current content 
-
-
-    const mainWrapper = document.createElement('div')
-    mainWrapper.id = 'main-wrapper'
-    mainWrapper.textContent = "Helloooo there"
-
-    return mainWrapper
+  return navBar;
 }
 
-export { loadNav, loadWrapper }
+function loadWrapper() {
+  //clear current content
+
+  const mainWrapper = document.createElement("div");
+  mainWrapper.id = "main-wrapper";
+
+  return mainWrapper;
+}
+
+export { loadNav, loadWrapper };
