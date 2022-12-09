7d9b5ac9911fda5ddb032a2bb4c60615d53ee50a
Samson Cheung <samsonc89@yahoo.com>
2022-12-09 11:58:57 -0800

    Add function to show which page is currently being viewed in navbar

--- src/modules/load.js
+++ src/modules/load.js
@@ -9,25 +9,42 @@ function loadNav() {
   const navBar = document.createElement("div");
   navBar.id = "navbar";
 
+  const navBtns = document.createElement("div");
+  navBtns.id = "navBtns";
+
+  const title = document.createElement("h2");
+  title.textContent = "Best Galbijjim";
+
   const homeButton = document.createElement("button");
   homeButton.classList.add("navButton");
   homeButton.textContent = "Home";
-  homeButton.addEventListener("click", loadHome);
+  homeButton.addEventListener("click", () => {
+    loadHome();
+    removeSelectedClass();
+  });
 
   const menuButton = document.createElement("button");
   menuButton.classList.add("navButton");
   menuButton.textContent = "Menu";
-  menuButton.addEventListener("click", loadMenu);
+  menuButton.addEventListener("click", () => {
+    loadMenu();
+    removeSelectedClass();
+  });
 
   const contactButton = document.createElement("button");
   contactButton.classList.add("navButton");
   contactButton.textContent = "Contact";
-  contactButton.addEventListener("click", loadContact);
+  contactButton.addEventListener("click", () => {
+    loadContact();
+    removeSelectedClass();
+  });
 
-  navBar.appendChild(homeButton);
-  navBar.appendChild(menuButton);
-  navBar.appendChild(contactButton);
-
+  navBtns.appendChild(homeButton);
+  navBtns.appendChild(menuButton);
+  navBtns.appendChild(contactButton);
+  navBar.appendChild(title);
+  navBar.appendChild(navBtns);
+  homeButton.classList.add("selected");
   return navBar;
 }
 
@@ -40,4 +57,10 @@ function loadWrapper() {
   return mainWrapper;
 }
 
-export { loadNav, loadWrapper };
+function removeSelectedClass() {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((btn) => btn.classList.remove("selected"));
+  event.target.classList.add("selected");
+}
+
+export { loadNav, loadWrapper, removeSelectedClass };
