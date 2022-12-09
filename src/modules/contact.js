1753f84ae84a9292e59b77c1339ea3c656d8978b
Samson Cheung <samsonc89@yahoo.com>
2022-12-09 10:38:54 -0800

    Add contact information

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,11 +1,18 @@
-function loadContact (){
+function loadContact() {
+  //clear current content
 
-    //clear current content 
+  const mainWrapper = document.querySelector("#main-wrapper");
+  mainWrapper.textContent = "";
 
+  const contactContent = document.createElement("div");
+  contactContent.id = "contact-content";
 
-    const mainWrapper = document.querySelector('#main-wrapper')
-    mainWrapper.textContent = "Contact testing"
+  contactContent.innerHTML = `
+  <p>📞: 123-456-7890</p>
+  <p>📧: bestgalbijjim@fake.com</p>
+  <p>📍: 123 Fake St, San Francisco, CA 12345</p>`;
 
+  mainWrapper.appendChild(contactContent);
 }
 
-export  {loadContact}
+export { loadContact };
